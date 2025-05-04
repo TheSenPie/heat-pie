6e7fa53f9be047c036289447f9b4a6567b638240
SenPie <smbat@duck.com>
2025-05-04 22:17:33 +0400

    Link raylib

--- src/hello.c
+++ src/hello.c
@@ -1,7 +1,28 @@
-#include <stdio.h>
+#include "raylib.h"
 
-int main()
+
+int main(void)
 {
-	fprintf( stdout, "Hello, World\n" );
+	const unsigned int screenWidth = 800u;
+	const unsigned int screenHeight = 450u;
+
+	InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window" );
+
+	SetTargetFPS( 60u );
+
+	// Main game loop
+	while ( !WindowShouldClose() )
+	{
+		BeginDrawing();
+			
+			ClearBackground( RAYWHITE );
+
+			DrawText( "Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY );
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+
 	return 0;
 }
